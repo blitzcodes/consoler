e1b37c41db7418ed24896c131635b2855ef8e5b0
Sean 'Blitz' Homer <blitzcodes@gmail.com>
2015-07-17 10:38:12 -0400

    Clean up and formatting.

--- consoler.js
+++ consoler.js
@@ -2,50 +2,51 @@
 /**
  * @module consoler
  */
- 
-/** 
+
+/**
  * A basic ES6 console fail safe wrapper
  */
 let consoler = {
-	/*************
-	 * Params
-	 ************/
-	enabled  : !!(window.console),
-	/*************
-	 * Private
-	 ************/
-	_msg(type, args) {
-		// If console isn't available, or no args were sent, or call type is missing, bypass
-		if(!consoler.enabled || !args.length || typeof window.console[type] !== 'function')
-			return;
-	
-		window.console[type].apply(window.console, args);
-	},
-	/*************
-	 * Public
-	 ************/
-	log() {
-		consoler._msg('log', arguments);
-	},
-	warn() {
-		consoler._msg('warn', arguments);
-	},
-	error() {
-		consoler._msg('error', arguments);
-	},
-	trace() {
-		consoler._msg('trace', arguments);
-	},
-	group(label) {
-		if(consoler.enabled && typeof window.console.groupEnd == 'function') {
-			window.console.groupCollapsed(label);
-		}
-	},
-	groupEnd : function(label) {
-		if(consoler.enabled && typeof console.groupEnd == 'function') {
-			window.console.groupEnd();
-		}
-	}
+  /*************
+   * Params
+   ************/
+  enabled: !!(window.console),
+  /*************
+   * Private
+   ************/
+    _msg(type, args) {
+    // If console isn't available, or no args were sent, or call type is missing, bypass
+    if (!this.enabled || !args.length || typeof window.console[type] !== 'function') {
+      return;
+    }
+
+    window.console[type].apply(window.console, args);
+  },
+  /*************
+   * Public
+   ************/
+    log() {
+    this._msg('log', arguments);
+  },
+  warn() {
+    this._msg('warn', arguments);
+  },
+  error() {
+    this._msg('error', arguments);
+  },
+  trace() {
+    this._msg('trace', arguments);
+  },
+  group(label) {
+    if (this.enabled && typeof window.console.groupEnd == 'function') {
+      window.console.groupCollapsed(label);
+    }
+  },
+  groupEnd: function (label) {
+    if (this.enabled && typeof console.groupEnd == 'function') {
+      window.console.groupEnd();
+    }
+  }
 };
 
 export default consoler;
